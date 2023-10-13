cae136bbcded26cecb012e538ec8273d90a2acb7
Prashaad <prashaadmail@gmail.com>
2023-10-13 22:32:09 +0530

    Add files via upload

---
+++ IntelliSprinklerAPI/Sprinkler/Data/DTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Sprinkler.Data
+{
+    public class Sprinkler
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ IntelliSprinklerAPI/Sprinkler/Data/DataStore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sprinkler.Data
+{
+    public static class DataStore
+    {
+        public static List<Sprinkler> Sprinklers = new List<Sprinkler>();
+    }
+}
